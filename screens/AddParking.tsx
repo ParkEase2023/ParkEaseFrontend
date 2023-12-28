e4f2500164ead4829795e11d5b6b78584c654ec8
Surawee kraikruan <67056659+sura1542@users.noreply.github.com>
2023-12-28 20:48:01 +0700

    20.47

--- screens/AddParking.tsx
+++ screens/AddParking.tsx
@@ -1,11 +1,22 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { Pressable } from 'react-native';
+import { KeyboardAwareScrollView } from'react-native-keyboard-aware-scroll-view';
+import { AuthTabParamList } from '../stack/AuthStack';
+import { ArrowLeft } from 'phosphor-react-native';
 
 const AddParking = () => {
+  const navigation = useNavigation<NativeStackNavigationProp<AuthTabParamList>>();
   return (
     <View>
       <Text>AddParking2</Text>
+      <Pressable onPress={() => navigation.goBack()}>
+        <ArrowLeft size={24} color="#565E8B" />
+      </Pressable>
     </View>
+    
   )
 }
 
