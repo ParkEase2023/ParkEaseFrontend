1bce80871798f0e59942c7ca9dbdbd38c7f03678
thanakrit410 <scms.dev01@gmail.com>
2024-02-01 10:57:21 +0700

    commit 10.57

--- screens/AddCoinQR.tsx
+++ screens/AddCoinQR.tsx
@@ -7,22 +7,70 @@ import {
     TextInput,
     Image,
     TouchableOpacity,
-    Alert
+    Alert,
+    PermissionsAndroid,
+    Platform
 } from 'react-native';
 import { CaretLeft, DownloadSimple } from 'phosphor-react-native';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { ProfileParamList } from '../stack/ProfileStack';
 import RNFS from 'react-native-fs';
+import RNFetchBlob from 'rn-fetch-blob';
 
 const AddCoinQR = () => {
     const navigation = useNavigation<NativeStackNavigationProp<ProfileParamList>>();
 
-    const saveImage = () => {
-        const img = require('../assets/IMGParking_1.jpg');
-        const filename = 'test.jpeg';
-        const picturesDirectoryPath = RNFS.PicturesDirectoryPath || RNFS.DocumentDirectoryPath;
-        RNFS.moveFile(img, `${picturesDirectoryPath}/${filename}`);
+    const saveImage = async () => {
+        let imgUrl =
+            'https://res.cloudinary.com/dqxh7vakw/image/upload/v1703827514/ParkEase/images_wu9kgt.jpg';
+
+        let newImgUri = imgUrl.lastIndexOf('/');
+        let imageName = imgUrl.substring(newImgUri);
+
+        let dirs = RNFetchBlob.fs.dirs;
+        let path =
+            Platform.OS === 'ios' ? dirs['MainBundleDir'] + imageName : dirs.PictureDir + imageName;
+
+        RNFetchBlob.config({
+            fileCache: true,
+            appendExt: 'png',
+            indicator: true,
+            IOSBackgroundTask: true,
+            path: path,
+            addAndroidDownloads: {
+                useDownloadManager: true,
+                notification: true,
+                path: path,
+                description: 'Image'
+            }
+        })
+            .fetch('GET', imgUrl)
+            .then(res => {
+                console.log(res, 'end downloaded');
+            });
+    };
+
+    const requestStoragePermission = async () => {
+        try {
+            const granted = await PermissionsAndroid.request(
+                PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+                {
+                    title: 'Storage Permission',
+                    message: 'App needs access to your storage to save the image.',
+                    buttonNeutral: 'Ask Me Later',
+                    buttonNegative: 'Cancel',
+                    buttonPositive: 'OK'
+                }
+            );
+            if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+                console.log('Storage permission granted');
+            } else {
+                console.log('Storage permission denied');
+            }
+        } catch (err) {
+            console.warn(err);
+        }
     };
 
     return (
