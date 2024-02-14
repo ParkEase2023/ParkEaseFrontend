d78cc1d72a4458e45cd0ee31235e5a8a983845d5
Palita Simaborvonsut <98012277+PattySoCute@users.noreply.github.com>
2024-02-14 16:14:45 +0700

    commit 16.14

--- screens/ApplyForMembership.tsx
+++ screens/ApplyForMembership.tsx
@@ -1,60 +1,79 @@
 import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import React from 'react';
 import { CaretLeft, CoinVertical, Check } from 'phosphor-react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 const ApplyForMembership = () => {
     return (
         <View style={styles.bg}>
             <View style={styles.rowTopic}>
-                <CaretLeft size={22} weight='bold' color='#EEF0FF'/>
+                <CaretLeft size={22} weight="bold" color="#EEF0FF" />
                 <Text style={styles.topic}>Apply For Membership</Text>
             </View>
-            <View style={styles.line}/>
+            <View style={styles.line} />
             <ScrollView style={styles.container}>
                 <View style={styles.member}>
                     <View style={styles.rowPrice}>
-                        <CoinVertical size={36} weight='fill' color='#FEFA94' />
+                        <CoinVertical size={36} weight="fill" color="#FEFA94" />
                         <Text style={styles.textPrice}>49</Text>
                         <Text style={styles.textMonth}>/mo</Text>
                     </View>
                     <Text style={styles.title}>Member</Text>
 
                     <View style={styles.rowCondition}>
-                        <Check size={20} weight='bold' color='#94FEBF'/>
+                        <Check size={20} weight="bold" color="#94FEBF" />
                         <Text style={styles.textCondition}>Can reservations</Text>
                     </View>
                     <View style={styles.rowCondition}>
-                        <Check size={20} weight='bold' color='#94FEBF'/>
+                        <Check size={20} weight="bold" color="#94FEBF" />
                         <Text style={styles.textCondition}>Can do transactions</Text>
                     </View>
                     <View style={styles.rowCondition}>
-                        <Check size={20} weight='bold' color='#94FEBF'/>
-                        <Text style={styles.textCondition}>Can add parking locations for information</Text>
+                        <Check size={20} weight="bold" color="#94FEBF" />
+                        <Text style={styles.textCondition}>
+                            Can add parking locations for information
+                        </Text>
                     </View>
 
-                    <TouchableOpacity style={styles.btnStart}>
-                        <Text style={styles.textStart}>GET STARTED NOW</Text>
+                    <TouchableOpacity>
+                        <LinearGradient
+                            start={{ x: 0, y: 0.5 }}
+                            end={{ x: 1, y: 0.5 }}
+                            colors={['#FEFA94', '#94FEBF', '#95EDFF']}
+                            style={styles.linearGradient}>
+                            <Text style={styles.textStart}>GET STARTED NOW</Text>
+                        </LinearGradient>
                     </TouchableOpacity>
                 </View>
                 <View style={styles.partner}>
                     <View style={styles.rowPrice}>
-                        <CoinVertical weight='fill' color='#FEFA94' />
+                        <CoinVertical weight="fill" color="#FEFA94" />
                         <Text style={styles.textPrice}>99</Text>
                         <Text style={styles.textMonth}>/mo</Text>
                     </View>
                     <Text style={styles.title}>Partner</Text>
 
                     <View style={styles.rowCondition}>
-                        <Check weight='bold' color='#94FEBF'/>
-                        <Text style={styles.textCondition}>Partner have the same rights as members.</Text>
+                        <Check weight="bold" color="#94FEBF" />
+                        <Text style={styles.textCondition}>
+                            Partner have the same rights as members.
+                        </Text>
                     </View>
                     <View style={styles.rowCondition}>
-                        <Check weight='bold' color='#94FEBF'/>
-                        <Text style={styles.textCondition}>Can add parking locations for booking</Text>
+                        <Check weight="bold" color="#94FEBF" />
+                        <Text style={styles.textCondition}>
+                            Can add parking locations for booking
+                        </Text>
                     </View>
 
-                    <TouchableOpacity style={styles.btnStart}>
-                        <Text style={styles.textStart}>GET STARTED NOW</Text>
+                    <TouchableOpacity>
+                        <LinearGradient
+                            start={{ x: 0, y: 0.5 }}
+                            end={{ x: 1, y: 0.5 }}
+                            colors={['#FEFA94', '#94FEBF', '#95EDFF']}
+                            style={styles.linearGradient}>
+                            <Text style={styles.textStart}>GET STARTED NOW</Text>
+                        </LinearGradient>
                     </TouchableOpacity>
                 </View>
             </ScrollView>
@@ -70,7 +89,7 @@ const styles = StyleSheet.create({
         flex: 1
     },
     container: {
-        padding: 16
+        padding: 25
     },
     rowTopic: {
         flexDirection: 'row',
@@ -83,11 +102,11 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         fontFamily: 'RedHatText-Bold',
         fontSize: 16,
-        color: '#EEF0FF',
+        color: '#EEF0FF'
     },
     line: {
         borderBottomColor: '#262D57',
-        borderBottomWidth: 1,
+        borderBottomWidth: 1
     },
     member: {
         backgroundColor: '#1E2139',
@@ -120,7 +139,8 @@ const styles = StyleSheet.create({
         fontFamily: 'RedHatText-Medium',
         fontSize: 24,
         color: '#DFE2F8',
-        marginTop: 16
+        marginTop: 16,
+        marginBottom: 8
     },
     rowCondition: {
         flexDirection: 'row',
@@ -133,11 +153,10 @@ const styles = StyleSheet.create({
         color: '#DFE2F8',
         marginLeft: 12
     },
-    btnStart: {
-        backgroundColor: '#FEFA94',
+    linearGradient: {
         borderRadius: 12,
         paddingVertical: 12,
-        marginTop: 16
+        marginTop: 20
     },
     textStart: {
         textAlign: 'center',
