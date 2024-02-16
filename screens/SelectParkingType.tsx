99dac44a1341c64ed6a31524e3a511d1b1cb197c
Palita Simaborvonsut <98012277+PattySoCute@users.noreply.github.com>
2024-02-16 20:06:35 +0700

    commit 20.06

--- screens/SelectParkingType.tsx
+++ screens/SelectParkingType.tsx
@@ -150,7 +150,7 @@ const styles = StyleSheet.create({
     btnNextStepActive: {
         marginHorizontal: 25,
         backgroundColor: '#10152F',
-        paddingVertical: 16,
+        paddingVertical: 18,
         alignItems: 'center',
         borderRadius: 16
     },
