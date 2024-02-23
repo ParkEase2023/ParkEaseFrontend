402992f5af7cb4c3ab4e08dcb53b3b6ba7da146b
thanakrit410 <scms.dev01@gmail.com>
2024-02-23 13:07:26 +0700

    commit 13.07

--- screens/BindAnAccount.tsx
+++ screens/BindAnAccount.tsx
@@ -10,20 +10,117 @@ import {
     Alert,
     KeyboardAvoidingView,
     Platform,
-    ScrollView
+    ScrollView,
+    Animated
 } from 'react-native';
-import { AirplaneInFlight, ArrowLeft, Bank, CaretDown, CaretLeft, CoinVertical, EnvelopeSimple, Eye, IdentificationCard, Money } from 'phosphor-react-native';
+import {
+    AirplaneInFlight,
+    ArrowLeft,
+    Bank,
+    CaretDown,
+    CaretLeft,
+    CoinVertical,
+    EnvelopeSimple,
+    Eye,
+    EyeSlash,
+    IdentificationCard,
+    Money
+} from 'phosphor-react-native';
 import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { ProfileParamList } from '../stack/ProfileStack';
 import { createdPromptPayQRCode } from '../services/omise';
+import TabSelectBank from '../components/TabSelectBank';
 
 const BindAnAccount = () => {
     const navigation = useNavigation<NativeStackNavigationProp<ProfileParamList>>();
     const { params } = useRoute<RouteProp<ProfileParamList, 'AddCoin'>>();
     const [inputNumber, setInputNumber] = useState<number>(0);
-    const handleConfirm = async () => {
+    const [isHidden, setIsHidden] = useState(true);
+    const translateY = new Animated.Value(100);
+    const [TaxID, setTaxID] = useState('');
+    const [selectBank, setSelectBank] = useState('');
+    const [isVisible, setIsVisible] = useState(false);
+    const [textEntry, setTextEntry] = useState(true);
+    const handleConfirm = async () => {};
+
+    useEffect(() => {
+        Animated.timing(translateY, {
+            toValue: isVisible ? 100 : 0, // Adjust the height as needed
+            duration: 500, // Adjust the duration as needed
+            useNativeDriver: true
+        }).start();
+    }, [isVisible, translateY, isHidden]);
+
+    const RenderTab = (): JSX.Element | null => {
+        if (isHidden === false) {
+            return (
+                <Animated.View
+                    style={{ ...styles.boxview, flex: 1, transform: [{ translateY: translateY }] }}>
+                    <TabSelectBank
+                        selectBank={value => {
+                            setSelectBank(value);
+                        }}
+                        setIsHidden={value => {
+                            setIsHidden(value);
+                        }}></TabSelectBank>
+                </Animated.View>
+            );
+        } else {
+            return null;
+        }
+    };
+
+    const handleOpenTab = () => {
+        if (isHidden === true) {
+            const duration = 100 * 1000;
+            setIsHidden(false);
+
+            const timer = setTimeout(() => {
+                setIsHidden(true);
+            }, duration);
+
+            return () => clearTimeout(timer);
+        } else {
+            setIsHidden(true);
+        }
+    };
+
+    const Renderbg = (): JSX.Element | null => {
+        if (isHidden === false) {
+            return (
+                <TouchableOpacity
+                    onPress={() => setIsHidden(true)}
+                    style={styles.containerpopup}></TouchableOpacity>
+            );
+        } else {
+            return null;
+        }
+    };
+
+    const Entrypassword = (): JSX.Element | null => {
+        if (textEntry == true) {
+            return (
+                <TouchableOpacity
+                    onPress={() => {
+                        setTextEntry(!textEntry);
+                        return false;
+                    }}>
+                    <EyeSlash size={24} weight="duotone" color="#565E8B" />
+                </TouchableOpacity>
+            );
+        } else {
+            return (
+                <TouchableOpacity
+                    onPress={() => {
+                        setTextEntry(!textEntry);
+                        return false;
+                    }}>
+                    <Eye size={24} weight="duotone" color="#565E8B" />
+                </TouchableOpacity>
+            );
+        }
     };
 
     return (
@@ -39,22 +136,22 @@ const BindAnAccount = () => {
                 </View>
                 <View style={styles.line}></View>
                 <View style={styles.mainContainer}>
-                   <Text style={styles.headerTextbody}>Payer's Information</Text>
-                    <View style={styles.row}>
-                            <View style={styles.textboxName}>
-                                <TextInput
-                                    style={styles.input}
-                                    placeholder="Name"
-                                    keyboardType= "email-address"
-                                />
-                            </View>
+                    <Text style={styles.headerTextbody}>Payer's Information</Text>
+                    <View style={styles.rowInputName}>
+                        <View style={styles.textboxName}>
+                            <TextInput
+                                style={styles.input}
+                                placeholder="Name"
+                                keyboardType="email-address"
+                            />
+                        </View>
                         <View style={styles.textboxLastname}>
                             <TextInput
                                 style={styles.input}
                                 placeholder="Last Name"
-                                keyboardType= "email-address"
+                                keyboardType="email-address"
                             />
-                        </View> 
+                        </View>
                     </View>
                     <View style={styles.space}>
                         <View style={[styles.textbox1]}>
@@ -62,26 +159,26 @@ const BindAnAccount = () => {
                                 <View style={styles.iconPosition}>
                                     <EnvelopeSimple size={32} />
                                 </View>
-                                    <TextInput
-                                        style={styles.inputBank}
-                                        placeholder="Email"
-                                        keyboardType= "email-address"
-                                    />
+                                <TextInput
+                                    style={styles.inputBank}
+                                    placeholder="Email"
+                                />
                             </View>
                         </View>
                     </View>
                     <View style={styles.space}>
                         <View style={[styles.textbox1]}>
-                            <View style={styles.iconPosition}>
-                                <View style={styles.row}>
+                            <View style={styles.row}>
+                                <View style={styles.iconPosition}>
                                     <IdentificationCard size={32} />
-                                        <TextInput
-                                            style={styles.input}
-                                            placeholder="Tax ID  (13-digit ID card number)"
-                                            keyboardType="email-address"
-                                        /> 
-                                        <Eye size={32} weight='fill' color='gray' />
                                 </View>
+                                <TextInput
+                                    style={styles.input}
+                                    secureTextEntry={textEntry}
+                                    placeholder="Tax ID  (13-digit ID card number)"
+                                    onChangeText={text => setTaxID(text)}
+                                />
+                                <Entrypassword></Entrypassword>
                             </View>
                         </View>
                     </View>
@@ -89,17 +186,19 @@ const BindAnAccount = () => {
                         <Text style={styles.headerTextbody}>Bank Account</Text>
                         <View style={[styles.textbox1]}>
                             <View style={styles.iconPosition}>
-                                <View style={styles.row}>
+                                <TouchableOpacity style={styles.row} onPress={handleOpenTab}>
                                     <Bank size={32} />
-                                        <TextInput
-                                            style={styles.inputBank}
-                                            placeholder="Choose a bank"
-                                            keyboardType="email-address"
-                                        />
-                                    <View >
+                                    <TextInput
+                                        style={styles.inputBank}
+                                        editable={false}
+                                        placeholder="Choose a bank"
+                                        value={selectBank}
+                                        keyboardType="email-address"
+                                    />
+                                    <View>
                                         <CaretDown size={32} />
                                     </View>
-                                </View>
+                                </TouchableOpacity>
                             </View>
                         </View>
                         <View style={styles.space}>
@@ -129,8 +228,10 @@ const BindAnAccount = () => {
                         <TouchableOpacity style={styles.btnConfirm} onPress={handleConfirm}>
                             <Text style={styles.textConfirm}>CONFIRM</Text>
                         </TouchableOpacity>
-                    </View>                   
+                    </View>
                 </View>
+                <RenderTab></RenderTab>
+                <Renderbg></Renderbg>
             </ScrollView>
         </KeyboardAvoidingView>
     );
@@ -174,8 +275,12 @@ const styles = StyleSheet.create({
     },
     row: {
         flexDirection: 'row',
+        alignItems: 'center'
+    },
+    rowInputName: {
+        flexDirection: 'row',
         alignItems: 'center',
-        justifyContent: 'space-between',
+        justifyContent: 'space-between'
     },
     iconPosition: {
         paddingHorizontal: 16
@@ -184,13 +289,13 @@ const styles = StyleSheet.create({
         backgroundColor: '#DAE0FF',
         borderRadius: 12,
         borderColor: '#10152F',
-        width: '48%',
+        width: '48%'
     },
     textboxLastname: {
         backgroundColor: '#DAE0FF',
         borderRadius: 12,
         borderColor: '#10152F',
-        width: '48%',
+        width: '48%'
     },
     boxText: {
         backgroundColor: '#565E8B',
@@ -209,10 +314,18 @@ const styles = StyleSheet.create({
     space: {
         marginTop: 20
     },
+    containerpopup: {
+        flex: 1,
+        backgroundColor: 'rgba(16, 21, 47, 0.8)',
+        height: 1000,
+        position: 'absolute',
+        width: '100%',
+        zindex: 3
+    },
     textbox1: {
         backgroundColor: '#DAE0FF',
         borderRadius: 12,
-        borderColor: '#10152F',
+        borderColor: '#10152F'
     },
     scrollViewContainer: {
         flexGrow: 1
@@ -222,6 +335,7 @@ const styles = StyleSheet.create({
         fontSize: 16,
         color: '#10152F',
         padding: 16,
+        flex: 9
     },
     inputBank: {
         fontFamily: 'RedHatText',
@@ -280,4 +394,11 @@ const styles = StyleSheet.create({
         borderWidth: 2,
         borderColor: '#7F85B2'
     },
+    boxview: {
+        width: '100%',
+        height: 550,
+        position: 'absolute',
+        bottom: 0,
+        zIndex: 2
+    }
 });
